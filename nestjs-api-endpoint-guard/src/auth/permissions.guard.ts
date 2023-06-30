0670bdc09f6927528c19edbf3b5679bbe8b59629
Filip Grebowski <filip@permit.io>
2023-06-30 12:14:21 +0200

    Nestjs project demo

---
+++ nestjs-api-endpoint-guard/src/auth/permissions.guard.ts
@@ -0,0 +1,37 @@
+// auth/permissions.guard.ts
+
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Permit } from 'permitio';
+
+// This line initializes the SDK and connects your app
+// to the Permit.io Cloud PDP.
+
+const permit = new Permit({
+    pdp: "https://cloudpdp.api.permit.io",
+    // your API Key
+    token: "permit_key_ILlNQIffvF39sp7bIFruzDVzfh8xBrm5Mw9JvyIYV8UEKE2GEs4ZUBoXZtJY3YKZcfKjmfQhrg2LqqxpaK0U1c",
+});
+
+@Injectable()
+export class PermissionsGuard implements CanActivate {
+  async canActivate(
+    context: ExecutionContext,
+  ): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    
+
+    // Add the authorization logic here with Permit.io.
+    // If the user has the necessary permissions, return true.
+    // If the user does not have the necessary permissions, throw an UnauthorizedException.
+
+    const userHasPermission = await permit.check("demo_user@gmail.com", "view", "protected-page");
+
+    console.log(userHasPermission);
+
+    if (!userHasPermission) {
+      throw new UnauthorizedException('You do not have the necessary permissions.');
+    }
+    return true;
+  }
+}
